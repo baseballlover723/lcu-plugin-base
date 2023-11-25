d5af5cfd6f7de9e598676afb527eaae5f02c6275
Maast3r <maastergotmail@gmail.com>
2023-11-24 18:36:41 -0800

    allow dynamic connecting/disconnecting of plugins

--- src/lcuPluginManager.js
+++ src/lcuPluginManager.js
@@ -2,9 +2,14 @@ import LCUConnector from 'lcu-connector';
 import RiotWebSocket from './riotWebSocket.js';
 
 export default class LcuPluginManager {
-  constructor(...plugins) {
+  constructor(plugins) {
+    if (!Array.isArray(plugins)) {
+      throw new Error('Must instantiate with an array of plugins.');
+    }
+
     this.connector = new LCUConnector();
     this.plugins = plugins;
+    this.ws = null;
 
     this.connector.on('connect', async (clientData) => {
       const onConnectPromises = [];
@@ -14,12 +19,12 @@ export default class LcuPluginManager {
       }
       console.log(`Waiting for ${this.plugins.length} plugins to setup`);
       await Promise.all(onConnectPromises);
-      const ws = new RiotWebSocket(clientData);
+      this.ws = new RiotWebSocket(clientData);
 
-      ws.on('open', () => {
+      this.ws.on('open', () => {
         for (const plugin of this.plugins) {
           for (const event in plugin.eventSubscriptions) {
-            ws.subscribe(event, plugin.eventSubscriptions[event]);
+            this.ws.subscribe(event, plugin.eventSubscriptions[event]);
           }
         }
         console.log('ready and listening for events');
@@ -35,10 +40,33 @@ export default class LcuPluginManager {
   }
 
   start() {
-    this.connector.start();
+    return this.connector.start();
   }
 
   stop() {
-    this.connector.stop();
+    return this.connector.stop();
+  }
+
+  connectPlugin(plugin) {
+    if (this.plugins.indexOf(plugin) === -1) {
+      this.plugins.push(plugin);
+      for (const event in plugin.eventSubscriptions) {
+        this.ws.subscribe(event, plugin.eventSubscriptions[event].bind(plugin));
+      }
+    } else {
+      console.error('Trying to connect a plugin that is already connected');
+    }
+  }
+
+  disconnectPlugin(plugin) {
+    const index = this.plugins.indexOf(plugin);
+    if (index > -1) {
+      for (const event in plugin.eventSubscriptions) {
+        this.ws.unsubscribe(event, plugin.eventSubscriptions[event].bind(plugin));
+      }
+      this.plugins = this.plugins.filter((connectedPlugin) => connectedPlugin !== plugin);
+    } else {
+      console.error('Trying to disconnect a plugin that is not connected');
+    }
   }
 }
