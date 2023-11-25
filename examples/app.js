357acda227f8386448065786a895fa30ec422633
Maast3r <maastergotmail@gmail.com>
2023-11-24 18:36:41 -0800

    allow dynamic connecting/disconnecting of plugins

--- examples/app.js
+++ examples/app.js
@@ -2,6 +2,15 @@ import LcuPluginManager from '../src/lcuPluginManager.js';
 import ExampleBasicLcuChatPlugin from './exampleBasicLcuChatPlugin.js';
 import ExampleAsyncSetupLcuChatPlugin from './exampleAsyncSetupLcuChatPlugin.js';
 
-const pluginManager = new LcuPluginManager(new ExampleBasicLcuChatPlugin(), new ExampleAsyncSetupLcuChatPlugin());
+const plugins = {
+    exampleChat: new ExampleBasicLcuChatPlugin(),
+    exampleAsync: new ExampleAsyncSetupLcuChatPlugin(),
+};
 
-pluginManager.start();
+const pluginManager = new LcuPluginManager(Object.values(plugins));
+
+await pluginManager.start();
+
+pluginManager.connectPlugin(plugins.exampleChat);
+pluginManager.disconnectPlugin(plugins.exampleAsync);
+console.log(pluginManager.plugins);
